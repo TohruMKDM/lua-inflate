bbe3431b059628314fe1fb662e6b4da2760c360f
RemRBLX <114203077+RemRBLX@users.noreply.github.com>
2022-09-22 22:56:37 -0400

    fix test.lua
    this make it use coro http and not ugly http library

--- tests.lua
+++ tests.lua
@@ -1,54 +1,26 @@
-local inflate = require('inflate')
-local http = require('https')
-
-local start, finish
-
-local files = {
-    ['zip_10MB/'] = false,
-    ['zip_10MB/file_example_JPG_1MB.jpg'] = false,
-    ['zip_10MB/file-example_PDF_1MB.pdf'] = false,
-    ['zip_10MB/file_example_ODS_5000.ods'] = false,
-    ['zip_10MB/file_example_TIFF_10MB.tiff'] = false,
-    ['zip_10MB/file_example_PNG_2500kB.jpg'] = false,
-    ['zip_10MB/file_example_PPT_1MB.ppt'] = false,
-    ['zip_10MB/file-sample_1MB.doc'] = false
-}
-
-http.get('https://files-example-com.github.io/uploads/zip_10MB.zip', function(response)
-    if response.statusCode ~= 200 then
-        print('Failed to get the sample ZIP archive.')
+local f = {["zip_10MB/"]=false,["zip_10MB/file_example_JPG_1MB.jpg"]=false,["zip_10MB/file-example_PDF_1MB.pdf"]=false,["zip_10MB/file_example_ODS_5000.ods"]=false,["zip_10MB/file_example_TIFF_10MB.tiff"]=false,["zip_10MB/file_example_PNG_2500kB.jpg"]=false,["zip_10MB/file_example_PPT_1MB.ppt"]=false,["zip_10MB/file-sample_1MB.doc"]=false}
+local r,b=require("coro-http").request("GET", "https://files-example-com.github.io/uploads/zip_10MB.zip")
+if r.code ~= 200 then print("Failed to get the sample ZIP archive.") return end
+local s=require("inflate"):new(b)
+local p,e=pcall(function()
+    local t=os.clock()
+    for n,o in s.files(s) do
+        f[n] = true
+        s.inflate(s, o)
+    end
+    t=os.clock()-t
+    print("Took "..t.."ms to traverse and inflate the ZIP archive")
+    s.unzip(s, "zip_10MB/file_example_JPG_1MB.jpg")
+end)
+if p==false then
+    print("error during inflate:"..e)
+    local _=process and process.exit(process, 0) or os.exit(0)
+end
+for n,w in pairs(f) do
+    if w==false then
+        print("File '"..n.."' not seen in ZIP file.")
+        local _=process and process.exit(process, 0) or os.exit(0)
         return
     end
-    local buffer = {}
-    response:on('data', function(chunk)
-        buffer[#buffer + 1] = chunk
-    end)
-    response:on('end', function()
-        local stream = inflate:new(table.concat(buffer))
-        local success, err = pcall(function()
-            start = os.clock()
-            for name, offset in stream:files() do
-                files[name] = true
-                stream:inflate(offset)
-            end
-            finish = os.clock()
-            print('Took '..(finish - start)..'ms to traverse and inflate the ZIP archive')
-            stream:unzip('zip_10MB/file_example_JPG_1MB.jpg')
-        end)
-        if not success then
-            print('Exception during inlation: '..err)
-            if process then
-                process:exit(0)
-            else
-                os.exit(0)
-            end
-        end
-        for name, seen in pairs(files) do
-            if not seen then
-                print('File "'..name..'" not seen in ZIP archive.')
-                return
-            end
-        end
-        print('Test passed.')
-    end)
-end)
+end
+print("Test pass.")
